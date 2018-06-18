7c6f5d9fc053e9ba451099c75f220e0c483011e3
kushtimusPrime <kushhari007@gmail.com>
2018-06-18 16:32:54 -0700

    Temporary login system is developed

--- app/src/main/java/com/example/kushtimusprime/cbusofficialtrialone/SignInPageActivity.java
+++ app/src/main/java/com/example/kushtimusprime/cbusofficialtrialone/SignInPageActivity.java
@@ -2,12 +2,32 @@ package com.example.kushtimusprime.cbusofficialtrialone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignInPageActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_page);
     }
+/*
+    Allows for Logins to occur
+ */
+    public void loginTry(View view) {
+        username=(EditText)findViewById(R.id.userNameEditText);
+        password=(EditText)findViewById(R.id.passwordEditText);
+        if(username.getText().toString().equals("user")&&password.getText().toString().equals("password")) {
+            Toast.makeText(this,"You got it",Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this,"You failed it",Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/kushtimusprime/cbusofficialtrialone/SignUpPageActivity.java
+++ app/src/main/java/com/example/kushtimusprime/cbusofficialtrialone/SignUpPageActivity.java
@@ -2,12 +2,15 @@ package com.example.kushtimusprime.cbusofficialtrialone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SignUpPageActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
     }
+
 }
