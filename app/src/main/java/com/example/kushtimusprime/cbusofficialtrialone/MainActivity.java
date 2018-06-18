565365e69ef570ca24cfd5abdf2dd014fd726d4b
kushtimusPrime <kushhari007@gmail.com>
2018-06-18 08:43:47 -0700

    Create sign in page when button is pressed

--- app/src/main/java/com/example/kushtimusprime/cbusofficialtrialone/MainActivity.java
+++ app/src/main/java/com/example/kushtimusprime/cbusofficialtrialone/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.kushtimusprime.cbusofficialtrialone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * @param view: Something required for this according to a tutorial
+     * Method opens a blank sign in page
+     */
+
+    public void openSignInPage(View view) {
+        Intent signInPage=new Intent(this,SignInPageActivity.class);
+        startActivity(signInPage);
     }
 }
---
+++ app/src/main/java/com/example/kushtimusprime/cbusofficialtrialone/SignInPageActivity.java
@@ -0,0 +1,13 @@
+package com.example.kushtimusprime.cbusofficialtrialone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignInPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_page);
+    }
+}
